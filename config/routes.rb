5779fa5781e733d03886573850a60549b8a0fd5c
Aki <aki@gmail.com>
2022-06-22 19:12:10 +0800

    Second commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :tasks
+  resources :buckets
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  #  root "tasks#index"
+   root "buckets#index"
+end
